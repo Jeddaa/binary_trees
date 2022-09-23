55ae3688b161320f7fdf05e74ae93e9b09bbe4cf
Jeddaa <fathiato@gmail.com>
2022-09-23 01:00:43 +0100

    binary trees correction

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,6 +15,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node, *temp;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node != NULL)
 	{
